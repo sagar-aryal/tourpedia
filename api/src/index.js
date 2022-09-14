9b8a1432df26ca40c9c068fd6abd09789ffb75ae
Sagar Aryal <sagar_yal92@outlook.com>
2022-09-14 09:25:44 +0300

    env file added to server to hide mongodb credientials

--- api/src/index.js
+++ api/src/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import morgan from "morgan";
+import dotenv from "dotenv";
 
 import userRouter from "./routes/user.js";
 import tourRouter from "./routes/tour.js";
 
 const app = express();
+dotenv.config();
 
 app.use(morgan("dev"));
 app.use(express.json({ limit: "30mb", extended: true }));
@@ -16,14 +18,10 @@ app.use(cors());
 app.use("/users", userRouter);
 app.use("/tours", tourRouter);
 
-const MONGODB_URL =
-  "mongodb+srv://codewithtodoapp:fullstacktodoapp@cluster0.ohvwrgz.mongodb.net/todo-db?retryWrites=true&w=majority";
-const PORT = 5000;
-
 mongoose
-  .connect(MONGODB_URL)
+  .connect(process.env.MONGODB_URL)
   .then(() => {
-    app.listen(PORT, () => {
+    app.listen(process.env.PORT, () => {
       console.log(
         `Backend is running successfully to database and running at http://localhost:${PORT}/`
       );
